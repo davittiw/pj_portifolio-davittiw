5980f9ba189c474ee21067bb429b3c30d61838ec
davittiw <lucctyte@gmail.com>
2025-04-07 16:57:01 -0300

    style: Adiciona responsividade para dispositivos até 625px

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
-}, {rootMargin: "0px 0px -10% 0px"})
+if (window.matchMedia("(min-width: 1400px)").matches) {
+    const myObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    }, { rootMargin: "0px 0px -50% 0px" });
 
-const elements = document.querySelectorAll('.hidden')
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach((element) => myObserver.observe(element));
 
-elements.forEach((element) => myObserver.observe(element))
+} else {
+    // Se a tela for menor que 450px, mostrar todos sem animação
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach((element) => {
+        element.classList.remove('hidden');
+        element.classList.add('show'); // Se quiser aplicar o estilo final direto
+    });
+}
